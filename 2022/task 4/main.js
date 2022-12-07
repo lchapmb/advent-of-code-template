53f11096f62992b589042eefcb028cf32d3fb06e
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-12-07 16:56:05 +0000

    task 4 done, 5 started

--- 2022/task 4/main.js
+++ 2022/task 4/main.js
@@ -12,36 +12,56 @@ const _TESTinput = inputToArray("../2022/task 4/test.txt", "\r\n");
 
 const part1 = (input) => {
   // part 1
-  let pairCount = 0;
-  console.log(input);
-  for (let i = 0; i < input.length; i++) {
-    let inputArr = input[i].split(",");
-    console.log(inputArr);
-    let elfOne = inputArr[0].split("-");
-    console.log(elfOne);
-    let elfTwo = inputArr[1].split("-");
-    console.log(elfTwo);
+  let overlapCount = 0;
 
-    if (elfOne[0] >= elfTwo[0] && elfOne[1] <= elfTwo[1]) {
-      console.log("1 in 2");
-      pairCount++;
-    } else if (elfTwo[0] >= elfOne[0] && elfTwo[1] <= elfOne[1]) {
-      console.log("2 in 1");
-      pairCount++;
+  for (let i = 0; i < input.length; i++) {
+    const drumOfElves = input[i].split(",");
+
+    const elfOne = drumOfElves[0].split("-").map((x) => parseInt(x));
+    const elfTwo = drumOfElves[1].split("-").map((x) => parseInt(x));
+
+    const elfOneInElfTwo = elfOne[0] >= elfTwo[0] && elfOne[1] <= elfTwo[1];
+    const elfTwoInElfOne = elfTwo[0] >= elfOne[0] && elfTwo[1] <= elfOne[1];
+
+    if (elfOneInElfTwo || elfTwoInElfOne) {
+      overlapCount++;
     }
   }
-  return pairCount;
+  return overlapCount;
 };
 
 const part2 = (input) => {
   // part
+  let overlapCount = 0;
 
-  return 0;
+  for (let i = 0; i < input.length; i++) {
+    const drumOfElves = input[i].split(",");
+
+    const elfOne = drumOfElves[0].split("-").map((x) => parseInt(x));
+    const elfTwo = drumOfElves[1].split("-").map((x) => parseInt(x));
+
+    const elfOneInElfTwo = elfOne[0] >= elfTwo[0] && elfOne[1] <= elfTwo[1];
+    const elfTwoInElfOne = elfTwo[0] >= elfOne[0] && elfTwo[1] <= elfOne[1];
+
+    const elfOneOverElfTwo = elfOne[0] <= elfTwo[0] && elfOne[1] >= elfTwo[0];
+    const elfTwoOverElfOne = elfOne[0] <= elfTwo[1] && elfOne[1] >= elfTwo[1];
+
+    if (
+      elfOneInElfTwo ||
+      elfTwoInElfOne ||
+      elfOneOverElfTwo ||
+      elfTwoOverElfOne
+    ) {
+      console.log("overlap");
+      overlapCount++;
+    }
+  }
+  return overlapCount;
 };
 
 tests([
-  test(part1, _TESTinput, 2),
-  // test(part1, _REALinput, 0),
-  // test(part2, _TESTinput, 0),
-  // test(part2, _REALinput, 0),
+  // test(part1, _TESTinput, 2),
+  // test(part1, _REALinput, 605),
+  test(part2, _TESTinput, 4),
+  test(part2, _REALinput, 914),
 ]);
--- 2022/task 5/main.js
+++ 2022/task 5/main.js
@@ -7,11 +7,29 @@
 
 const { inputToArray, test, tests } = require("../../common/common");
 
-// const _REALinput = inputToArray("../2022/task 2/input.txt", "\r\n");
-// const _TESTinput = inputToArray("../2022/task 2/test.txt", "\r\n");
+// const _REALinput = inputToArray("../2022/task 5/input.txt", "\r\n");
+const _TESTinput = inputToArray("../2022/task 5/test.txt", "\r\n");
 
 const part1 = (input) => {
   // part 1
+  const gap = input.indexOf("");
+  const boxStartState = input.slice(0, gap - 1);
+  // console.log(boxStartState);
+  const boxLabels = input[gap - 1];
+  console.log(boxLabels.length);
+  const instructions = input.slice(gap + 1);
+  // console.log(instructions);
+
+  for (let i = boxStartState.length - 1; i >= 0; i--) {
+    let row = boxStartState[i];
+    console.log(row);
+    let col = [];
+    for (let j = 1; j < row.length; j += 4) {
+      console.log(row[j]);
+      col.push(row[j]);
+    }
+    console.log(col);
+  }
 
   return 0;
 };
@@ -23,7 +41,7 @@ const part2 = (input) => {
 };
 
 tests([
-  // test(part1, _TESTinput, 0),
+  test(part1, _TESTinput, "CMZ"),
   // test(part1, _REALinput, 0),
   // test(part2, _TESTinput, 0),
   // test(part2, _REALinput, 0),
