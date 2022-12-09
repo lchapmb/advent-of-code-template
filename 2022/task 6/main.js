7893b6c1dff7d51b5948af5c950172ca61c5e5c5
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-12-09 16:36:43 +0000

    task 6 done

--- 2022/task 6/main.js
+++ 2022/task 6/main.js
@@ -7,24 +7,49 @@
 
 const { inputToArray, test, tests } = require("../../common/common");
 
-// const _REALinput = inputToArray("../2022/task 2/input.txt", "\r\n");
-// const _TESTinput = inputToArray("../2022/task 2/test.txt", "\r\n");
+const _REALinput = inputToArray("../2022/task 6/input.txt", "");
+const _TESTinput = inputToArray("../2022/task 6/test.txt", "");
+
+const getMarkerByLength = (input, length) => {
+  for (let i = 3; i < input.length; i++) {
+    let tempArr = [];
+
+    for (let j = 0; j < length; j++) {
+      tempArr.push(input[i - j]);
+    }
+
+    let dupsInArr = false;
+    tempArr.forEach((x) => {
+      const count = tempArr.filter((y) => x === y).length;
+      if (count > 1) {
+        dupsInArr = true;
+      }
+    });
+
+    if (!dupsInArr) {
+      markerFoundAt = i + 1;
+      break;
+    }
+  }
+
+  return markerFoundAt;
+};
 
 const part1 = (input) => {
   // part 1
 
-  return 0;
+  return getMarkerByLength(input, 4);
 };
 
 const part2 = (input) => {
   // part 2
 
-  return 0;
+  return getMarkerByLength(input, 14);
 };
 
 tests([
-  // test(part1, _TESTinput, 0),
-  // test(part1, _REALinput, 0),
-  // test(part2, _TESTinput, 0),
-  // test(part2, _REALinput, 0),
+  test(part1, _TESTinput, 7),
+  test(part1, _REALinput, 1625),
+  test(part2, _TESTinput, 19),
+  test(part2, _REALinput, 2250),
 ]);
